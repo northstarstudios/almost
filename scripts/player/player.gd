873d11ea97d5e36d63b01ad477be0b77ae575ad2
code2344 <71059013+code2344@users.noreply.github.com>
2026-08-23 21:45:32 +1000

    player movement

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,11 +1,15 @@
-extends Node
+extends CharacterBody2D
 
+@export var speed: float = 120.0
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	var direction := Input.get_vector(
+		"move_left",
+		"move_right",
+		"move_up",
+		"move_down"
+	)
+	
+	velocity = direction * speed
+	move_and_slide()
