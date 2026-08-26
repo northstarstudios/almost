e21dba2a537e429cd2919c12f75dc651923ba9a5
code2344 <71059013+code2344@users.noreply.github.com>
2026-08-26 13:08:40 +1000

    persist the first begin action

--- scripts/game_state.gd
+++ scripts/game_state.gd
@@ -1,6 +1,28 @@
 extends Node
 
+const BEGIN_MARKERS := [
+	"user://almost/session.dat",
+	"user://almost/recovery.dat",
+	"user://almost/archive.dat",
+]
+
 var loop_count := 0
+var has_begun := false
+
+func _ready() -> void:
+	for marker_path in BEGIN_MARKERS:
+		if FileAccess.file_exists(marker_path):
+			has_begun = true
+			return
+
+func record_begin() -> void:
+	has_begun = true
+	DirAccess.make_dir_recursive_absolute("user://almost")
+
+	for marker_path in BEGIN_MARKERS:
+		var marker := FileAccess.open(marker_path, FileAccess.WRITE)
+		if marker != null:
+			marker.store_string("has_begun=true\n")
 
 func complete_reset() -> void:
 	loop_count += 1
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -3,9 +3,13 @@ extends Control
 const FIRST_ROOM := "res://scenes/loading_scene.tscn"
 
 func _ready() -> void:
+	if GameState.has_begun:
+		$CenterContainer/VBoxContainer/NewGameButton.text = "BEGIN AGAIN"
+
 	$CenterContainer/VBoxContainer/NewGameButton.grab_focus()
 
 func _on_new_game_button_pressed() -> void:
+	GameState.record_begin()
 	get_tree().change_scene_to_file(FIRST_ROOM)
 
 func _on_quit_button_pressed() -> void:
