07e2e1dd1ad56ed7460d5d83699dc123537be6c9
code2344 <71059013+code2344@users.noreply.github.com>
2026-08-26 12:23:49 +1000

    separate player interaction from movement collision

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -2,8 +2,9 @@ extends CharacterBody2D
 
 @export var speed: float = 120.0
 
-# Called when the node enters the scene tree for the first time.
-func _physics_process(delta: float) -> void:
+@onready var interaction_hitbox: Area2D = $InteractionHitbox
+
+func _physics_process(_delta: float) -> void:
 	var direction := Input.get_vector(
 		"move_left",
 		"move_right",
@@ -13,3 +14,13 @@ func _physics_process(delta: float) -> void:
 	
 	velocity = direction * speed
 	move_and_slide()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if not event.is_action_pressed("interact") or event.is_echo():
+		return
+
+	for area in interaction_hitbox.get_overlapping_areas():
+		if area.has_method("interact"):
+			area.interact()
+			get_viewport().set_input_as_handled()
+			return
